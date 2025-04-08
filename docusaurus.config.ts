2a8b790e186923f77144c0cd193dd71d63527c75
swyth_ <swyth+github@pm.me>
2025-04-08 19:00:19 +0200

    fix: baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://swyth-dev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/medhead-architecture-repository/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
